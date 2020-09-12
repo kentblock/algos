42d344ba0c1ca22c6e1d2b8b4fd669c5bbb7fd1d
Riley Peel <rpeel4@outlook.com>
2020-09-12 11:36:08 -0700

    clean up on RatInAMaze.py

--- backtracking/RatInAMaze.py
+++ backtracking/RatInAMaze.py
@@ -1,20 +1,13 @@
-import heapq, random, math
-
 # Problem: Given an NxN Matrix representing a maze (0's represent blocked path, 1's open path) find a solution if it exists
 #   - Simple version where source and destination are (0, 0) and (n-1, n-1) respectively
+# BUG random method seems to try too many moves on some unsolvable mazes
 
-# Approach 1: Basic Backtracking algorithm
-
-# Approach 2: Backtracking with a heuristic
-
-# Possible heuristics: 
-#   - Euclidean Distance to destination (Doesn't work very well)
-#   - Choose the next position with the 
-
-
+import heapq, random, math
 
 def generate_random_maze(size):
-    
+    """
+    Generate a random maze, which may or may not be solvable
+    """
     maze = []
     for _ in range(size):
         row = []
@@ -24,75 +17,90 @@ def generate_random_maze(size):
                 rand_int = 0
             row.append(rand_int)
         maze.append(row)
-
     maze[0][0] = 0
     maze[size - 1][size - 1] = 0
     return maze
 
 def clear_maze(maze):
-    
+    """
+    Clear the rat's path from the maze
+    """
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             if maze[i][j] is -1:
                 maze[i][j] = 0
 
+def is_valid(maze, pos, move):
+    """
+    return True if move is valid for pos on maze
+    """
+    return in_bounds(pos, move) and is_open(maze, move_rat(pos, move))
 
-def is_valid(pos, move):
+def is_open(maze, pos):
+    """
+    return True if position is open
+    """
+    return maze[pos[0]][pos[1]] == 0
+
+def in_bounds(pos, move):
+    """
+    return True if move from pos is still in the maze
+    """
     return ((pos[0] + move[0]) >= 0 and (pos[1] + move[1]) >= 0 and 
         (pos[0] + move[0]) < MAZE_SIZE and (pos[1] + move[1]) < MAZE_SIZE)
 
-def move(pos, move):
+def move_rat(pos, move):
+    """
+    return new position after making move
+    """
     return (pos[0] + move[0], pos[1] + move[1])
 
 def find_path(maze, position):
-
+    """
+    Find path in maze recursively if there is one, tries moving down and right first always
+    """
     global iteration_counter
     iteration_counter += 1
     if position == DESTINATION:
         return True
 
     for m in rat_moves:
-        if is_valid(position, m):
-            new_position = move(position, m)
-            if maze[new_position[0]][new_position[1]] == 0:
-                maze[new_position[0]][new_position[1]] = -1
-                path_found = find_path(maze, new_position)
-                if path_found:
-                    return path_found
-                maze[new_position[0]][new_position[1]] = 0
+        if is_valid(maze, position, m):
+            new_position = move_rat(position, m)
+            maze[new_position[0]][new_position[1]] = -1
+            path_found = find_path(maze, new_position)
+            if path_found:
+                return path_found
+            maze[new_position[0]][new_position[1]] = 0
 
     return False
 
-def get_distance_to_destination(position, destination):
-    return math.sqrt((destination[0] - position[0])^2 + (destination[1] - position[1])^2)
-
-def find_path_heuristic(maze, position):
-    
-    global iteration_counter_1
-    iteration_counter_1 += 1
+def find_path_random(maze, position):
+    """
+    Find path in maze recursively if there is one, tries moves in random order
+    """
+    global iteration_counter
+    iteration_counter += 1
     if position == DESTINATION:
         return True
+    moves = [m for m in rat_moves]
+    random.shuffle(moves)
 
-    distance_pq = []
-    for m in rat_moves:
-        if is_valid(position, m):
-            new_position = move(position, m)
-            if maze[new_position[0]][new_position[1]] == 0:
-                dist = get_distance_to_destination(position, DESTINATION)
-                heapq.heappush(distance_pq, (dist, new_position))
-
-    while distance_pq:
-        new_position = heapq.heappop(distance_pq)[1]
-        maze[new_position[0]][new_position[1]] = -1
-        path_found = find_path_heuristic(maze, new_position)
-        if path_found:
-            return True
-        maze[new_position[0]][new_position[1]] = 0
+    for m in moves:
+        if is_valid(maze, position, m):
+            new_position = move_rat(position, m)
+            maze[new_position[0]][new_position[1]] = -1
+            path_found = find_path_random(maze, new_position)
+            if path_found:
+                return path_found
+            maze[new_position[0]][new_position[1]] = 0
 
     return False
 
 def print_formatted_maze(maze):
-
+    """
+    Print the maze in fairly readable format
+    """
     for row in maze:
         for val in row:
             space = " "
@@ -101,32 +109,42 @@ def print_formatted_maze(maze):
             print(f"{space}{val}", end="")
         print("")
 
-if __name__ == "__main__":
-
-    iteration_counter = 0
-    rat_moves = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    MAZE_SIZE = 10
-    SOURCE = (0, 0)
-    DESTINATION = (MAZE_SIZE - 1, MAZE_SIZE - 1)
-    
-    maze = generate_random_maze(MAZE_SIZE)
-    print_formatted_maze(maze)
+def solve_maze(func, maze):
+    """
+    Given a func to solve maze, find a solution if there is one
+    and output the result
+    """
     print("")
     maze[SOURCE[0]][SOURCE[1]] = -1
-    result = find_path(maze, SOURCE)
+    result = func(maze, SOURCE)
     print(f"Number of moves tried: {iteration_counter}")
     if result:
         print_formatted_maze(maze)
+        print("")
     else:
-        print("No solution for the maze.")
+        print("No solution for the maze.\n")
 
-    clear_maze(maze)
-    print("")
-    iteration_counter_1 = 0
-    maze[SOURCE[0]][SOURCE[1]] = -1
-    result = find_path_heuristic(maze, SOURCE)
-    print(f"Number of moves tried: {iteration_counter_1}")
-    if result:
+if __name__ == "__main__":
+    """
+    TESTING
+    """
+    iteration_counter = 0
+    rat_moves = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    MAZE_SIZE = 10
+    SOURCE = (0, 0)
+    DESTINATION = (MAZE_SIZE - 1, MAZE_SIZE - 1)
+    random_total = 0
+    down_right_total = 0
+    for _ in range(10):
+        maze = generate_random_maze(MAZE_SIZE)
         print_formatted_maze(maze)
-    else:
-        print("No solution for the maze.")
+        solve_maze(find_path, maze)
+        down_right_total += iteration_counter
+        iteration_counter = 0
+        clear_maze(maze)
+        solve_maze(find_path_random, maze)
+        random_total += iteration_counter
+    
+    comparison_ratio = (down_right_total - random_total) / down_right_total * 100
+    print(f"""Always moving down and right first performed on average 
+        {comparison_ratio}% better than picking a direction at random""")
