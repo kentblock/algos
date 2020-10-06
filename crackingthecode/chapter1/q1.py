1cf1f58059a9eced76c476883a2166ec8a7ae8b0
Riley Peel <rpeel4@outlook.com>
2020-10-06 11:04:38 -0700

    chapter1 q1 solution

---
+++ crackingthecode/chapter1/q1.py
@@ -0,0 +1,30 @@
+# Cracking the code q 1.1 Is Unique
+
+
+def is_unique(input_string):
+    """
+    Return True if all characters in string are unique,
+    otherwise False, using no additional data structures
+    """
+    
+    if len(input_string) > 26:
+        return False
+
+    input_string = input_string.lower()
+    ascii_shift = ord('a')
+
+    bit_vector = 0
+    for char in input_string:
+        ord_char = ord(char) - ascii_shift
+        if 2 ** ord_char & bit_vector == 2 ** ord_char:
+            return False
+        bit_vector = 2 ** ord_char | bit_vector
+    return True
+
+if __name__ == "__main__":
+    """
+    TESTING
+    """
+
+    assert is_unique('abcdefghijklmn')
+    assert not is_unique('abcddefg')
