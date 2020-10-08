257de610bb21e60d6e13828c86c27cb65ebb89a3
Riley Peel <rpeel4@outlook.com>
2020-10-08 08:53:38 -0700

    solution for 1.6

---
+++ crackingthecode/chapter1/q6.py
@@ -0,0 +1,34 @@
+# Cracking the code question 1.6 String Compression
+
+
+def str_compression(input_str):
+    """
+    Return compressed string if it is shorter than original otherwise
+    return the original string
+    """
+    reps = 1
+    compress_str = ""
+    for i in range(len(input_str)):
+        if i is len(input_str) - 1:
+            compress_str = f"{compress_str}{input_str[i]}{reps}"
+            break
+        if input_str[i + 1] != input_str[i]:
+            compress_str = f"{compress_str}{input_str[i]}{reps}"
+            reps = 1
+        else:
+            reps += 1
+    
+    if len(compress_str) < len(input_str):
+        return compress_str
+
+    return input_str
+
+
+if __name__ == "__main__":
+    """
+    Testing
+    """
+
+    print(str_compression("aabccccaaa"))
+    print(str_compression("abcd"))
+    print(str_compression("bbccdde"))
